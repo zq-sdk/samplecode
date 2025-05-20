52716829f24d26b591920e1642968e20650d333b
ElegantLee <superlee.handso@gmail.com>
2025-05-20 16:00:03 +0800

    chore(iot): 配置element-plus

--- iot/src/App.vue
+++ iot/src/App.vue
@@ -1,18 +1,20 @@
 <script setup>
-import { inject, ref, onMounted } from "vue";
-import SceneContainer from "./components/SceneContainer.vue";
+import { inject, ref, onMounted } from 'vue'
+import SceneContainer from './components/SceneContainer.vue'
 
-const space = inject("space");
-const isInit = ref(false);
+const space = inject('space')
+const isInit = ref(false)
 
 onMounted(async () => {
-  await space.init();
-  isInit.value = true;
-});
+  await space.init()
+  isInit.value = true
+})
 </script>
 
 <template>
-  <SceneContainer v-if="isInit" />
+  <el-config-provider size="small" :z-index="3000">
+    <SceneContainer v-if="isInit" />
+  </el-config-provider>
 </template>
 
 <style scoped></style>
--- iot/vite.config.js
+++ iot/vite.config.js
@@ -1,41 +1,50 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig(() => {
-	return {
-		base: './',
-		build: {
-			outDir: './dist',
-			assetsDir: 'assets',
-			assetsInlineLimit: 4096,
-			rollupOptions: {
-				output: {
-					assetFileNames(assetInfo) {
-						const assetsFileNamesMap = new Map([
-							[/.(png|jpg|jpeg|gif|svg|webp)$/, '/img/[name].[ext]'],
-							[/.css$/, '/css/[name]-[hash].[ext]'],
-							[/.(mp3|mp4)$/, '/media/[name]-[hash].[ext]'],
-							[/.(woff2|woff|eot|ttf)$/, '/font/[name].[ext]']
-						]);
+  return {
+    base: './',
+    build: {
+      outDir: './dist',
+      assetsDir: 'assets',
+      assetsInlineLimit: 4096,
+      rollupOptions: {
+        output: {
+          assetFileNames(assetInfo) {
+            const assetsFileNamesMap = new Map([
+              [/.(png|jpg|jpeg|gif|svg|webp)$/, '/img/[name].[ext]'],
+              [/.css$/, '/css/[name]-[hash].[ext]'],
+              [/.(mp3|mp4)$/, '/media/[name]-[hash].[ext]'],
+              [/.(woff2|woff|eot|ttf)$/, '/font/[name].[ext]'],
+            ])
 
-						for (const [reg, value] of assetsFileNamesMap.entries()) {
-							if (reg.test(assetInfo.name)) return `assets${value}`;
-						}
+            for (const [reg, value] of assetsFileNamesMap.entries()) {
+              if (reg.test(assetInfo.name)) return `assets${value}`
+            }
 
-						return 'assets/[ext]/[name]-[hash].[ext]';
-					},
-					entryFileNames: 'assets/js/index.js',
-				}
-			}
-		},
-		resolve: {
-			alias: {
-				'@': resolve(__dirname, './src'),
-			},
-		},
-		plugins: [
-			vue()
-		]
-	};
-});
+            return 'assets/[ext]/[name]-[hash].[ext]'
+          },
+          entryFileNames: 'assets/js/index.js',
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  }
+})
